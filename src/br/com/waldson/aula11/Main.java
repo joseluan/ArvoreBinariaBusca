da87ff4c5a15b1a491dfcbde97adc8f91d3a85cd
joseluan <joseluansilva@outlook.com>
2019-09-17 23:27:38 -0300

    Remocao conclluida

---
+++ src/br/com/waldson/aula11/Main.java
@@ -0,0 +1,17 @@
+package br.com.waldson.aula11;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArvoreBinariaBusca arvore = new ArvoreBinariaBusca();
+
+        for (int i = 0; i < 5; i++) {
+            arvore.insert(
+                new Node(i)
+            );
+        }
+        
+        System.out.println(arvore.getMaxFolha().getValue());
+        System.out.println(arvore.remove(9).getValue());
+    }
+}
